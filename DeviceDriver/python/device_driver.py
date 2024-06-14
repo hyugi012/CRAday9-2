237cba5d3e667498be762cc5f9a0e5799a51f8e5
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-14 15:17:22 +0900

    [Feature] Read, Write test case

--- DeviceDriver/python/device_driver.py
+++ DeviceDriver/python/device_driver.py
@@ -1,5 +1,6 @@
 from hardware_interface import FlashMemoryDevice
 
+
 class DeviceDriver:
     """
     This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
@@ -13,8 +14,15 @@ class DeviceDriver:
 
     def write(self, address: int, data: int) -> None:
         # TODO: implement this method
-        pass
+        if self.__device.read(address) != 0xFF:
+            raise Exception("WriteFailException")
+        self.__device.write(address, data)
+        return None
 
     def read(self, address: int) -> int:
         # TODO: implement this method
-        return 0
+        value = self.__device.read(address)
+        for i in range(5):
+            if value != self.__device.read(address):
+                raise Exception("Read Fail Exception")
+        return value
--- DeviceDriver/python/test_device_driver.py
+++ DeviceDriver/python/test_device_driver.py
@@ -1,10 +1,20 @@
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 from hardware_interface import FlashMemoryDevice
 from device_driver import DeviceDriver
+
 
 class DeviceDriverTest(TestCase):
     def test_successful_read(self):
         hardware: FlashMemoryDevice = Mock()
+        hardware.read.return_value = 0x5
         driver = DeviceDriver(hardware)
-        self.assertEqual(0x0, driver.read(0xFF))
+
+        self.assertEqual(0x5, driver.read(0xFF))
+
+    def test_successful_write(self):
+        hardware: FlashMemoryDevice = Mock()
+        hardware.read.return_value = 0xFF
+        driver = DeviceDriver(hardware)
+
+        self.assertIsNone(driver.write(0x1, 0x1))
